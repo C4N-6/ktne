cc827394583aa4cec07f9d69fdaf1c5b1e565345
C4N-6 <patetoooman@gmail.com>
2026-03-15 13:20:29 -0400

    added third maze

--- Mazes/maze.c
+++ Mazes/maze.c
@@ -26,7 +26,18 @@ const Maze possible_mazes[9] = {{{{false, false, true, false, false},
                                   {false, true, false, true, false, false},
                                   {false, false, false, false, true, false}},
                                  {{4, 1}, {1, 3}}},
-                                {{}, {}, {{}, {}}},
+                                {{{false, false, true, true, false},
+                                  {true, true, true, false, true},
+                                  {false, true, true, false, true},
+                                  {true, true, true, true, true},
+                                  {true, false, true, true, true},
+                                  {false, false, false, true, false}},
+                                 {{false, true, false, false, false, false},
+                                  {true, false, false, true, true, false},
+                                  {false, false, false, false, false, false},
+                                  {false, false, false, false, false, false},
+                                  {false, true, true, false, false, false}},
+                                 {{3, 3}, {5, 3}}},
                                 {{}, {}, {{}, {}}},
                                 {{}, {}, {{}, {}}},
                                 {{}, {}, {{}, {}}},
@@ -60,7 +71,7 @@ void init_maze(MazeDisplay *maze) {
   }
   maze->win = newwin(side_length, side_length, y_pos, x_pos);
 
-  maze->current_maze = &possible_mazes[1];
+  maze->current_maze = &possible_mazes[2];
 }
 
 void draw_maze(MazeDisplay *maze) {
